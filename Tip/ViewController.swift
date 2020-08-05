7383ae86cadecf27cc026c6c7fefcb9f67a2ab30
Erik Villavera <villaveraerik@gmail.com>
2020-08-05 15:54:04 -0400

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Erik Villavera on 8/5/20.
+//  Copyright © 2020 Erik Villavera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
