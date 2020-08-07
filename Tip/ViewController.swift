ae39ba2571f66af1ac1b74917e586884ce6b7525
Erik Villavera <villaveraerik@gmail.com>
2020-08-06 21:25:16 -0400

    added basic setup and custom tip

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var customTextField: UITextField!
+    
+    @IBOutlet weak var segmentedTipControl: UISegmentedControl!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountTextField.becomeFirstResponder()
     }
     
-    //hello world
-
-
+    @IBAction func onTap(_ sender: Any) {
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = (Double(billAmountTextField.text!) ?? 0)
+        let tipArray = [0.15, 0.18, 0.2, 0]
+        
+        
+        var tip = bill * tipArray[segmentedTipControl.selectedSegmentIndex]
+        if(segmentedTipControl.selectedSegmentIndex == 3){
+            tip = (bill * (Double(customTextField.text!) ?? 0)) / 100
+        }
+        let total = tip + bill
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalAmountLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
